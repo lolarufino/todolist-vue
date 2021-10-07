99ae05beba86a552c58387e93d182dd2500ef7b3
Lola Rufino <lola.ruf.ar@gmail.com>
2021-10-07 16:08:40 +0200

    Set up eslint, vetur and prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,28 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    browser: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module',
   },
   extends: [
+    'airbnb-base',
     'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
+    'prettier/vue',
+    'plugin:prettier/recommended'
   ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'comma-dangle': 'off',
+    'class-methods-use-this': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'import/prefer-default-export': 'off',
+    "vue/component-name-in-template-casing": ["error", "kebab-case", {
+      "ignores": []
+    }],
+    'prettier/prettier': ['error', { 'singleQuote': true, 'endOfLine': 'auto' }]
   },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
-      env: {
-        jest: true,
-      },
-    },
-  ],
 };
