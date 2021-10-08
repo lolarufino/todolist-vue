8e79e639080ac2ac48a8d7e1f64cf2ace70a2a91
Lola Rufino <lola.ruf.ar@gmail.com>
2021-10-08 19:03:09 +0200

    Create store with create and delete actions

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,35 @@
 import { createStore } from 'vuex';
+import axios from 'axios';
 
 export default createStore({
   state: {
+    toDos: [{}],
   },
   mutations: {
+    loadToDos(state, payload){
+      state.toDos = payload;
+    },
+    addNewToDo(state, payload){
+      state.toDos.push(payload);
+    },
+    updateToDos(state, payload){
+      state.toDos = state.toDos.filter((toDo: any) => toDo.title !== payload);
+    }
   },
   actions: {
+    async fetchToDos ({commit}): Promise<void>{
+      const {data} = await axios.get('https://jsonplaceholder.typicode.com/todos')
+
+      commit('loadToDos', data);
+    },
+    async saveToDo ({commit}, inputValue): Promise<void>{
+      const data = {title: inputValue};
+      commit("addNewToDo", data);
+    },
+    async deleteToDoChosen ({commit}, {title}):Promise<void>{
+      const chosenToDo = title;
+      commit("updateToDos", chosenToDo);
+    }
   },
   modules: {
   },
