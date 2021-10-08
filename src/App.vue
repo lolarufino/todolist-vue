80c909f8da4a795b598052cab180edb96ca67321
Lola Rufino <lola.ruf.ar@gmail.com>
2021-10-08 19:03:47 +0200

    Create basic template for to do list

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,44 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <h1>To Do List</h1>
+  <input type="text" v-model="inputValue" />
+  <button @click="saveValue()">Create task</button>
+  <button>Update task</button>
+  <ul>
+    <li v-for="toDo in toDos">
+      {{ toDo.title
+      }}<button @click="deleteToDoChosen({ title: toDo.title })">X</button>
+    </li>
+  </ul>
 </template>
 
+<script lang="ts">
+import { defineComponent } from "vue";
+import { mapActions, mapState } from "vuex";
+
+export default defineComponent({
+  name: "App",
+  computed: {
+    ...mapState(["toDos"]),
+  },
+  methods: {
+    ...mapActions(["fetchToDos", "saveToDo", "deleteToDoChosen"]),
+    saveValue() {
+      if (this.inputValue !== "") {
+        this.saveToDo(this.inputValue);
+      }
+    },
+  },
+  data() {
+    return {
+      inputValue: "",
+    };
+  },
+  mounted() {
+    this.fetchToDos();
+  },
+});
+</script>
+
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
